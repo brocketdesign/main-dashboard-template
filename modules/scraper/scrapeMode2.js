750db9a01be1e9a4e39d658548b49c1bf828cd82
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2023-09-25 02:58:39 +0900

    add scrpaer

--- modules/scraper/scrapeMode2.js
+++ modules/scraper/scrapeMode2.js
@@ -5,7 +5,6 @@ const puppeteer = require('puppeteer');
 async function scrapeMode2(url, mode, nsfw, page, filter = 'images'){
   const data1 = await scrapeReddit(url, mode, nsfw, page, filter = 'images');
   const data2 = await scrapeScrolller(url, mode, nsfw, page)
-  console.log(data2)
   const data = data1.concat(data2)
   return data
 }
@@ -14,42 +13,44 @@ const scrapeScrolller = (subreddit, mode, nsfw, page) => {
     try {
       const url = `https://scrolller.com${subreddit}/`;
 
+      // Launch Puppeteer browser
       const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu']
       });
 
+      // Create a new page
       const page = await browser.newPage();
+
+      // Navigate to the URL
       await page.goto(url, { waitUntil: 'networkidle2' });
-      await page.evaluate(() => {
-        window['SCROLLLER_BETA_1:CONFIRMED_NSFW'] = true;
-      });
-
-      const scrapedData = await page.evaluate((url, subreddit, mode, nsfw) => {
-        const items = Array.from(document.querySelectorAll('.vertical-view__item-container'));
-        const data = items.map(item => {
-          try {
-            const link = 'https://scrolller.com'+item.querySelector('a').getAttribute('href');
-            const video_id = new ObjectId();
-            const imageUrl = item.querySelector('img').getAttribute('src');
-            const currentPage = url;
-  
-            return { video_id, imageUrl, link ,currentPage, query:subreddit, subreddit, mode, nsfw, extractor:'scrolller' };
-          } catch (error) {
-            console.log(error)
-          }
-        });
-        return data;
-      }, url, subreddit, mode, nsfw);
-
-      await browser.close();
       
+      // Wait for the element with the specified class to appear
+      await page.waitForSelector('.nsfw-warning__accept-button');
+
+      // Click on the element by class
+      await page.click('.nsfw-warning__accept-button');
+
+      const itemSelector = '.vertical-view__item-container'; // Replace with your item selector
+      const itemCount = 50; // Number of items you want to collect
+    
+      const scrapedData = await scrollAndScrape(page, itemSelector, itemCount);
+    
+      // Log the total number of scraped items
+      console.log('Total scraped items:', scrapedData.length);
+
+      // Close the browser
+      await browser.close();
+
+      // Resolve with the scraped data
       resolve(scrapedData);
     } catch (error) {
+      // Reject if there's an error
       reject(error);
     }
   });
 }
+
 async function scrapeReddit(url, mode, nsfw, page, filter = 'images') {
   if (!url) return false;
 
@@ -97,6 +98,38 @@ async function scrapeReddit(url, mode, nsfw, page, filter = 'images') {
     return [];
   }
 }
+async function scrollAndScrape(page, itemSelector, itemCount) {
+  const scrapedData = [];
+  const uniqueItems = new Set(); // Using a Set to automatically avoid duplicate objects
+
+  while (scrapedData.length < itemCount) {
+    // Scroll the page down by one viewport height
+    await page.evaluate(() => {
+      window.scrollBy(0, window.innerHeight);
+    });
+
+    // Get the updated list of items
+    const items = await page.$$eval(itemSelector, elements => {
+      return elements.map(element => {
+        const link = 'https://scrolller.com' + element.querySelector('a').getAttribute('href');
+        const thumb = element.querySelector('img').getAttribute('src');
+        return { link, thumb, extractor :'scrolller' };
+      });
+    });
+
+    // Filter and add unique items to the scrapedData array
+    items.forEach(item => {
+      const key = JSON.stringify(item); // Create a unique key for the object
+      if (!uniqueItems.has(key)) {
+        uniqueItems.add(key);
+        scrapedData.push(item);
+      }
+    });
+  }
+
+  return scrapedData; // Return the collected data
+}
+
 
 function filterData(data, filter, nsfw) {
   return data.reduce((filteredData, post) => {
